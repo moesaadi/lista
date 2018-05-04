cff5bb372e1f88a8525d41c81a0b521c513eef9a
Moe Saadi <moe.saadi@icloud.com>
2018-05-05 02:00:50 +0300

    Initial Commit

---
+++ lista/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lista
+//
+//  Created by Moe Saadi on 05/05/2018.
+//  Copyright © 2018 Moe Saadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
