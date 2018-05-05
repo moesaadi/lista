9e85fdf2ef9220673a4668c6c6c1d668cb1b5409
Moe Saadi <moe.saadi@icloud.com>
2018-05-05 05:22:04 +0300

    coding RealmSwift finished saving adding and checking but not deleting yet, will try to jump query

--- lista/Controllers/ToDoListViewController.swift
+++ lista/Controllers/ToDoListViewController.swift
@@ -52,12 +52,17 @@ class ToDoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        //        context.delete(itemArray[indexPath.row])
-        //        itemArray.remove(at: indexPath.row)
+        if let item = todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    item.done = !item.done
+                }
+            } catch {
+                print("Error saving done status,\(error)")
+            }
+        }
         
-//        todoItems[indexPath.row].done = !todoItems[indexPath.row].done
-//
-//        saveItems()
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
